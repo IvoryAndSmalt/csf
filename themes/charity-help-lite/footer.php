f4c83b6cc85d67dcc59899216d74adab61f0ea58
anjum18 <anjumandleeb@gmail.com>
2019-02-18 09:50:18 +0100

    add child theme

--- themes/charity-help-lite/footer.php
+++ themes/charity-help-lite/footer.php
@@ -11,10 +11,10 @@
 <div class="footer-copyright">
         	<div class="container">
             	<div class="copyright-txt">
-					<?php bloginfo('name'); ?>. <?php esc_html_e('All Rights Reserved', 'charity-help-lite');?>           
+					<?php bloginfo('name'); ?>. <?php esc_html_e('All Rights Reserved', 'Cravanche sans frontières');?>           
                 </div>
                 <div class="design-by">
-				  <a href="<?php echo esc_url( __( 'https://gracethemes.com/themes/charity-help-lite/', 'charity-help-lite' ) ); ?>"><?php printf( __( 'Theme by %s', 'charity-help-lite' ), 'Grace Themes' ); ?></a>
+				  <a href="<?php echo esc_url( __( 'https://gracethemes.com/themes/charity-help-lite/', 'Cravanche sans frontières' ) ); ?>"><?php printf( __( 'Theme by %s', 'charity-help-lite' ), 'Grace Themes' ); ?></a>
                 </div>
                  <div class="clear"></div>
             </div>           
