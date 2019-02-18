f025cb8d61e2a525c241a887254e133e555331dd
Lucas <lucas180395@gmail.com>
2019-02-18 12:01:23 +0100

    enqueue scripts

--- themes/charity_child/functions.php
+++ themes/charity_child/functions.php
@@ -2,10 +2,13 @@
 /**
 ** activation theme
 **/
+
 function theme_enqueue_styles() {
  wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
- wp_enqueue_script('script.js', get_stylesheet_directory_uri() . '/../charity-help-lite/js/editable.js');
+
+ wp_enqueue_script( 'editable.js', get_stylesheet_directory_uri().'/script.js',null, null, true);
 }
 
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 
+//  var_dump(get_stylesheet_directory_uri().'/script.js');
