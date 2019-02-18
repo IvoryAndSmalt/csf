2948ce338bd94d85710a57e3527b1f4872d7198b
Krez90 <bilel.hamimed@yahoo.fr>
2019-02-18 13:32:54 +0100

    riendutout

--- themes/charity_child/functions.php
+++ themes/charity_child/functions.php
@@ -2,8 +2,18 @@
 /**
 ** activation theme
 **/
-add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles', 'add_theme_scripts' );
 function theme_enqueue_styles() {
  wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 
 }
+
+
+function add_theme_scripts() {
+    wp_enqueue_script( 'script', get_template_directory_uri() . '/js/script.js', array ( 'jquery' ), 1.1, true);
+   
+      if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+        wp_enqueue_script( 'comment-reply' );
+      }
+  }
+  
