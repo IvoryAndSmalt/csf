51a10c7ec7955c3fb2c734decc2763eca7f2dc36
rainbow59 <laura.vangrevenynghe@laposte.net>
2019-02-22 14:53:12 +0100

    Laura -> recuperation footer

---
+++ themes/charity_child/footer.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after
+ *
+ * @package Charity Help Lite
+ */
+?>
+
+<div class="footer-copyright">
+        	<div class="container">
+            	<div class="contact">
+                    <h3>Contact</h3>
+                    <p>Téléphone : </p>
+                    <p>assocravanche@yahoo.com</p>
+                    <p>13 Val du Salbert - 90300 CRAVANCHE</p>
+                    <a href="https://cravanche.promo-24.codeur.online/?page_id=84">Formulaire de contact</a>
+                </div>
+                <div class="carteSvg">
+                    <img id="svg" src="http://localhost/csf/wp-content/themes/charity_child/MAP/MAP.svg" alt="SVG de la carte du monde"/>
+                </div>
+                <div class="menu">
+                    <h3>Menu</h3>
+                    <a href="https://cravanche.promo-24.codeur.online/">Accueil</a>
+                    <a href="https://cravanche.promo-24.codeur.online/?page_id=62">Qui sommes-nous ?</a>
+                    <a href="https://cravanche.promo-24.codeur.online/?page_id=20">Missions</a>
+                    <a href="https://cravanche.promo-24.codeur.online/?page_id=91">Gallerie</a>
+                    <a href="">Adhésion</a>
+                    <a href="https://cravanche.promo-24.codeur.online/?page_id=53">Faire un don</a>
+                </div>
+            </div>           
+        </div>        
+</div><!--#end site-holder-->
+
+<?php wp_footer(); ?>
+</body>
+</html>
--- themes/charity_child/functions.php
+++ themes/charity_child/functions.php
@@ -3,12 +3,8 @@
 ** activation theme
 **/
 
-function theme_enqueue_styles() {
- wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-
- wp_enqueue_script( 'editable.js', get_stylesheet_directory_uri().'/script.js',null, null, true);
-}
-
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 
-//  var_dump(get_stylesheet_directory_uri().'/script.js');
+function theme_enqueue_styles() {
+ wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+}
